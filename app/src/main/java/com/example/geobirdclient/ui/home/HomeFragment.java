d885edacf5452b175a9658f48c526fcc302756f8
Kinga <kinga2233@onet.pl>
2021-06-01 23:11:18 +0200

    popraawki,logo

--- app/src/main/java/com/example/geobirdclient/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/geobirdclient/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +15,40 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.geobirdclient.R;
+import com.example.geobirdclient.models.User.User;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button instructionButton;
+    private Button informationsButton;
+    private Button informationsButton2;
+    private Button galleryButton;
+    private Button yourAccountButton;
+    private ImageView imageMainView;
+    private Button adminEditable;
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-       /* final TextView textView ;//= root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-               // textView.setText(s);
-            }
-        });*/
+
+        instructionButton = root.findViewById(R.id.instructionButton);
+        informationsButton = root.findViewById(R.id.informationsbutton);
+        informationsButton2 = root.findViewById(R.id.informations_button_2);
+        galleryButton = root.findViewById(R.id.galleryButton);
+        yourAccountButton = root.findViewById(R.id.your_account_button);
+        imageMainView = root.findViewById(R.id.lama_in_my_livingroom);
+        adminEditable = root.findViewById(R.id.adminEditable);
+        adminEditable.setVisibility(View.INVISIBLE);
+
+        //TODO: dodac zalozenie pojawiajacego kafelka po zalogowaniu i znalezieniu ADMINA
+
+
         return root;
     }
 }
