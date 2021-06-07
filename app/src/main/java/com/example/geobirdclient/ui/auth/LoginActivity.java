80405a62a5e39287e9426f87f1c7aa7aef3c6e26
Kamil Rękas <zekse00@gmail.com>
2021-06-07 18:33:58 +0200

    Implements RegisterActivity

--- app/src/main/java/com/example/geobirdclient/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/geobirdclient/ui/auth/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.example.geobirdclient.ui.auth;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.geobirdclient.MainActivity;
 import com.example.geobirdclient.R;
 import com.example.geobirdclient.api.Api;
@@ -29,6 +32,9 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button goRegisterButton;
     private ImageView image;
+    private Activity activity;
+    private String login;
+    private String password;
     ModalWidgets modalWidgets = new ModalWidgets(this);
 
 
@@ -40,40 +46,66 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButton);
         goRegisterButton = findViewById(R.id.goRegisterButton);
         image =findViewById(R.id.imageView);
-        Glide.with(activity).load(target.getImage()).into(targetImageView);
+        Glide.with(this).load(getResources().getDrawable(R.drawable.logo_bird)).into(image);
         addActions();
     }
 
     private void addActions() {
         loginButton.setOnClickListener(view -> {
-            String login = textLogin.getEditText().getText().toString();
-            String password = textPassword.getEditText().getText().toString();
-            UserService service = Api.getRetrofit().create(UserService.class);
-            Call<UserResponse> call = service.loginUser(new UserLogin(login,password));
-            call.enqueue(new Callback<UserResponse>() {
-                @Override
-                public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
-                    UserResponse userResponse = response.body();
-                    if(userResponse !=null){
-                        User user = userResponse.getUser();
+            login = textLogin.getEditText().getText().toString();
+            password = textPassword.getEditText().getText().toString();
+
+            if(checkLogin()){
+                UserService service = Api.getRetrofit().create(UserService.class);
+                Call<UserResponse> call = service.loginUser(new UserLogin(login,password));
+                call.enqueue(new Callback<UserResponse>() {
+                    @Override
+                    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                        UserResponse userResponse = response.body();
+                        if(userResponse !=null){
+                            User user = userResponse.getUser();
                             System.out.println("Zalogowano: "+user.getNickname());
                             saveUserCredentials(login,password);
                             redirectToMain(user);
-                    } else {
-                        modalWidgets.showToast(getString(R.string.incorrect_login_data));
+                        } else {
+                            modalWidgets.showToast(getString(R.string.incorrect_login_data));
+                        }
                     }
-                }
 
-                @Override
-                public void onFailure(Call<UserResponse> call, Throwable t) {
-                    System.out.println("Cos poszlo nie tak: "+ t.getMessage());
-                }
-            });
+                    @Override
+                    public void onFailure(Call<UserResponse> call, Throwable t) {
+                        System.out.println("Cos poszlo nie tak: "+ t.getMessage());
+                    }
+                });
+            }
+
         });
 
         goRegisterButton.setOnClickListener(view ->{
             redirectToRegister();
         });
+    }
+
+    private boolean checkLogin(){
+        if(login.equals("")){
+            textLogin.setError(getString(R.string.login_is_empty));
+            textLogin.setErrorEnabled(true);
+        }else{
+            textLogin.setErrorEnabled(false);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkPassword(){
+        if(password.equals("")){
+            textPassword.setError(getString(R.string.password_empty));
+            textPassword.setErrorEnabled(true);
+        }else{
+            textPassword.setErrorEnabled(false);
+            return true;
+        }
+        return false;
     }
 
     public void saveUserCredentials(String login, String password) {
--- app/src/main/java/com/example/geobirdclient/ui/auth/RegisterActivity.java
+++ app/src/main/java/com/example/geobirdclient/ui/auth/RegisterActivity.java
@@ -3,11 +3,14 @@ package com.example.geobirdclient.ui.auth;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.geobirdclient.MainActivity;
 import com.example.geobirdclient.R;
 import com.example.geobirdclient.api.Api;
@@ -32,6 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout textRepeat;
     private TextInputLayout textEmail;
     private Button registerButton;
+    private ImageView image;
 
     private String password;
     private String passwordRepeat;
@@ -52,6 +56,8 @@ public class RegisterActivity extends AppCompatActivity {
         textRepeat = findViewById(R.id.passwordRepeatTextField);
         textEmail = findViewById(R.id.emailTextField);
         registerButton = findViewById(R.id.registerButton);
+        image = findViewById(R.id.imageView);
+        Glide.with(this).load(getResources().getDrawable(R.drawable.logo_bird)).into(image);
         addActions();
     }
 
@@ -64,20 +70,27 @@ public class RegisterActivity extends AppCompatActivity {
             passwordRepeat = textRepeat.getEditText().getText().toString();
             email = textEmail.getEditText().getText().toString();
 
-            if(checkMail()){
+            boolean chM = checkMail();
+            boolean chP = checkPassword();
+            boolean chL = checkLogin();
+            if(chM && chP && chL){
                 UserService service = Api.getRetrofit().create(UserService.class);
-                Call<UserResponse> call = service.registerUser(new UserRegister(email,login,password));
+                Call<UserResponse> call = service.registerUser(new UserRegister(email,password,login));
                 call.enqueue(new Callback<UserResponse>() {
                     @Override
                     public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
                         UserResponse userResponse = response.body();
                         if(userResponse !=null){
-                            User user = userResponse.getUser();
-                            System.out.println("Zarejestrowano: "+user.getNickname());
-                            saveUserCredentials(login,password);
-                            redirectToMain(user);
+                            if(userResponse.getUser() != null){
+                                User user = userResponse.getUser();
+                                System.out.println("Zarejestrowano: "+user.getNickname());
+                                saveUserCredentials(login,password);
+                                redirectToMain(user);
+                            } else {
+                                modalWidgets.showToast(getString(R.string.cannot_register_data));
+                            }
                         } else {
-                            modalWidgets.showToast(getString(R.string.incorrect_login_data));
+                            modalWidgets.showToast(getString(R.string.error));
                         }
                     }
                     @Override
@@ -104,13 +117,27 @@ public class RegisterActivity extends AppCompatActivity {
         return false;
     }
 
+    private boolean checkLogin(){
+        if(login.equals("")){
+            textLogin.setError(getString(R.string.login_is_empty));
+            textLogin.setErrorEnabled(true);
+        }else{
+            textLogin.setErrorEnabled(false);
+            return true;
+        }
+        return false;
+    }
+
     private boolean checkPassword(){
-        if(password.equals(passwordRepeat)){
+        if(!password.equals(passwordRepeat)){
             textPassword.setError(getString(R.string.password_not_same));
             textPassword.setErrorEnabled(true);
-        }else{
-            textPassword.setErrorEnabled(false);
-            return true;
+        }else if(password.equals("")){
+            textPassword.setError(getString(R.string.password_empty));
+            textPassword.setErrorEnabled(true);
+        } else {
+                textPassword.setErrorEnabled(false);
+                return true;
         }
         return false;
     }
