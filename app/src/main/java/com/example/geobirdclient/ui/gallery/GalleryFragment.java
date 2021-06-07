52aa37ab7b2814619c62b0c09b3dc3a40f3cfae9
Kamil Rękas <zekse00@gmail.com>
2021-06-07 19:37:02 +0200

    add adapter

--- app/src/main/java/com/example/geobirdclient/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/geobirdclient/ui/gallery/GalleryFragment.java
@@ -12,30 +12,66 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 
+import com.example.geobirdclient.MainActivity;
 import com.example.geobirdclient.R;
+import com.example.geobirdclient.api.Api;
+import com.example.geobirdclient.api.UserService;
+import com.example.geobirdclient.api.UserTargetService;
 import com.example.geobirdclient.api.models.Target.Target;
+import com.example.geobirdclient.api.models.User.User;
+import com.example.geobirdclient.api.models.User.UserLogin;
+import com.example.geobirdclient.api.models.User.UserResponse;
+import com.example.geobirdclient.api.models.usertarget.UserTarget;
+import com.example.geobirdclient.api.models.usertarget.UserTargetGetByResponse;
+import com.example.geobirdclient.api.models.usertarget.UserTargetGetByUser;
+import com.example.geobirdclient.ui.auth.ModalWidgets;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GalleryFragment extends Fragment {
 
     private GridView galleryGridView;
     private GalleryViewModel galleryViewModel;
+    private List<UserTarget> targetList = new ArrayList<>();
+    private ModalWidgets modalWidgets = new ModalWidgets(getActivity());
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery,container,false);
 
-        Target[] targetList;
+
+        UserTargetService service = Api.getRetrofit().create(UserTargetService.class);
+        Call<UserTargetGetByResponse> call = service.getUserTargetByUser(new UserTargetGetByUser(MainActivity.currentUser.getId()));
+        call.enqueue(new Callback<UserTargetGetByResponse>() {
+            @Override
+            public void onResponse(Call<UserTargetGetByResponse> call, Response<UserTargetGetByResponse> response) {
+                UserTargetGetByResponse userTargetGetByResponse = response.body();
+                if(userTargetGetByResponse !=null){
+                    targetList = userTargetGetByResponse.getUserTargetDataDTO();
+                    System.out.println("zaladowano: " + targetList.toString());
+                } else {
+                    modalWidgets.showToast(getString(R.string.incorrect_login_data));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserTargetGetByResponse> call, Throwable t) {
+                System.out.println("Cos poszlo nie tak: "+ t.getMessage());
+            }
+        });
 
         ArrayAdapter<Target> cheeseAdapter =
                 new ArrayAdapter<Target>(getActivity(),
                         R.layout.item_character_gallery,
                         R.id.image_target,
                         R.id.title_name_target,
-                        targetList
+                        targetList[0]
                 );
 
         galleryGridView = root.findViewById(R.id.grid_view_gallery);
