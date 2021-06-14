a63c4c654e496b3c91fb76ac62609f18bb254e27
Kamil Rękas <zekse00@gmail.com>
2021-06-14 20:59:49 +0200

    add

--- app/src/main/java/com/example/geobirdclient/ui/map/MapFragment.java
+++ app/src/main/java/com/example/geobirdclient/ui/map/MapFragment.java
@@ -1,19 +1,25 @@
 package com.example.geobirdclient.ui.map;
 
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.geobirdclient.R;
 import com.example.geobirdclient.api.Api;
@@ -21,9 +27,11 @@ import com.example.geobirdclient.api.TargetService;
 import com.example.geobirdclient.api.models.Target.Target;
 import com.example.geobirdclient.api.models.Target.TargetResponse;
 import com.example.geobirdclient.ui.qr.QrScanFragment;
-import com.example.geobirdclient.ui.qr.QrScanViewModel;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -31,15 +39,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MapFragment extends AppCompatActivity implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    private GoogleMap mapView;
+    private Activity activity;
+    private MapViewModel mViewModel;
+    private MapView mapView;
     GoogleMap googleMap;
     private Button findNewObject;
     private Button scanobject;
@@ -50,21 +59,26 @@ public class MapFragment extends AppCompatActivity implements OnMapReadyCallback
     protected String latitude,longitude;
     protected boolean gps_enabled,network_enabled;
 
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
 
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_map);
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
-        findNewObject = findViewById(R.id.find_new_possition);
-        scanobject = findViewById(R.id.scanner_view);
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        activity = getActivity();
+        mapView =  root.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
         addAction();
+        return root;
     }
 
     private void addAction() {
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -81,23 +95,23 @@ public class MapFragment extends AppCompatActivity implements OnMapReadyCallback
         //locationManager.GPS_PROVIDER. https://developer.android.com/reference/android/location/LocationManager#GPS_PROVIDER
 
 
-        scanobject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(this, QrScanFragment.class);
-                startActivity(intent);
-            }
-        });
+
 
 //https://javapapers.com/android/get-current-location-in-android/
     }
-//https://developer.android.com/training/location/change-location-settings
 
-
-
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+    }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        MapsInitializer.initialize(this.getActivity());
+
+        System.out.println("XFDD");
         //pobieranie danych dot. celow
         TargetService targetService = Api.getRetrofit().create(TargetService.class);
         Call<List<Target>> call = targetService.getTargets();
@@ -105,20 +119,20 @@ public class MapFragment extends AppCompatActivity implements OnMapReadyCallback
         call.enqueue(new Callback<List<Target>>() {
             @Override
             public void onResponse(Call<List<Target>> call, Response<List<Target>> response) {
-                TargetResponse targetResponse = (TargetResponse) response.body();
-                List<Target> targetList = (List<Target>) targetResponse.getTargetX();
+                List<Target> targetList = response.body();
                 System.out.println("pokaz targets: " + targetList);
 
-                mapView = googleMap;
                 List<LatLng> latLngO = new ArrayList<>();
 
 
-                for (int i = 0; i <= targetList.size(); i++){
+                for (int i = 0; i < targetList.size(); i++){
                     LatLng location = new LatLng(targetList.get(i).getLocX(), targetList.get(i).getLocY());
                     latLngO.add(location);
                 }
 // dodawanie punktow na mapie
-                mapView.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
                         for (int i = 0; i <= latLngO.size(); i++){
@@ -126,9 +140,9 @@ public class MapFragment extends AppCompatActivity implements OnMapReadyCallback
                             MarkerOptions markerOptions = new MarkerOptions();
                             markerOptions.position(latLng);
                             markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                            mapView.clear();
-                            mapView.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                            mapView.addMarker(markerOptions);
+                            googleMap.clear();
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            googleMap.addMarker(markerOptions);
                         }
                     }
                 });
@@ -141,7 +155,4 @@ public class MapFragment extends AppCompatActivity implements OnMapReadyCallback
             }
         });
     }
-
-///https://www.youtube.com/watch?v=2ppri1ovIQA
-
 }
